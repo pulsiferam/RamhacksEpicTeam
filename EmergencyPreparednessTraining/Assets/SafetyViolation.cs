0e6b50f5adeadaddf5b8c51a4f39844a44b5d402
Michael Poblacion <Mpoblacion92@gmail.com>
2018-09-22 23:12:22 -0400

    Safety Hazard Awareness complete

---
+++ EmergencyPreparednessTraining/Assets/SafetyViolation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+public class SafetyViolation : MonoBehaviour {
+
+    public Text uiText;
+    public int numberOfSafetyViolations;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Here");
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnMouseDown()
+    {
+        GameObject[] violations = GameObject.FindGameObjectsWithTag("SafetyViolation");
+        numberOfSafetyViolations = violations.Length -1;
+
+        if (gameObject.tag == "SafetyViolation")
+        {
+            DestroyImmediate(this.gameObject);
+        }
+
+        if (numberOfSafetyViolations == 0)
+        {
+            uiText.text = "Level complete! FIRE DRILL! Exit the room.";
+            GameObject newObject = new GameObject();
+        }
+        else
+        {
+            uiText.text = "Crises averted! " + numberOfSafetyViolations + " safety items remaining";
+        }
+    }
+}
