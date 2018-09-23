44af12dedc5904e76d980682aa91d345e4cd784b
pulsiferam <pulsiferam@vcu.edu>
2018-09-23 05:59:08 -0400

    emergencykit scene
    Ruchi's change

---
+++ EmergencyPreparednessTraining/Assets/objectTable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objectTable : MonoBehaviour {
+    public Text textui;
+    // Use this for initialization
+    public void Start()
+    {
+        textui.text = "Pick object for your emergency kit";
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "good")
+        {
+            Debug.Log("collided");
+            // Destroy(col.gameObject);
+            textui.text = "Good Job";
+        }
+        else
+            textui.text = "You might die!";
+    }
+
+}
